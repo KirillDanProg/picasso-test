195104f99a17750d93227ff4c2b65804346f8ba5
KirillDanProg <kirill2898v@gmail.com>
2024-04-03 09:11:18 +0300

    feat: store, store provider, base api added

---
+++ src/app/providers/store-provider/ui/index.tsx
@@ -0,0 +1,9 @@
+import { Provider } from "react-redux";
+import { store } from "../config/store";
+
+type Props = {
+  children: React.ReactNode;
+};
+export const StoreProvider = ({ children }: Props) => {
+  return <Provider store={store}>{children}</Provider>;
+};
