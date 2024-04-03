c0b914eeccfeb8651753505cc6cff2463c009fea
KirillDanProg <kirill2898v@gmail.com>
2024-04-03 09:14:51 +0300

    refactor: css baseline , container added

--- src/pages/main-page/ui/MainPage.tsx
+++ src/pages/main-page/ui/MainPage.tsx
@@ -1,11 +1,16 @@
 import { Header } from "@/widgets/header";
 import { Outlet } from "react-router-dom";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const MainPage = () => {
   return (
     <>
+      <CssBaseline />
       <Header />
-      <Outlet />
+      <Container maxWidth="md" style={{ height: "100vh" }}>
+        <Outlet />
+      </Container>
     </>
   );
 };
