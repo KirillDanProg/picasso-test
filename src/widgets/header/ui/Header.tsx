9aad62e0e69be94751a490bba041bcbf898d943e
KirillDanProg <kirill2898v@gmail.com>
2024-04-03 20:51:46 +0300

    refactor: logo added to header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,4 +1,15 @@
+import { Typography } from "@mui/material";
 import s from "./Header.module.scss";
 export const Header = () => {
-  return <header className={s.header}>Header</header>;
+  return (
+    <header className={s.header}>
+      <Typography
+        fontWeight={"bold"}
+        fontSize={"1.5rem"}
+        sx={{ textTransform: "uppercase", color: "#df496b" }}
+      >
+        пикассо тест
+      </Typography>
+    </header>
+  );
 };
