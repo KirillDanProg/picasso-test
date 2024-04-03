1199fdbd6a0dbfb50bf93e5d816f95914374c720
KirillDanProg <kirill2898v@gmail.com>
2024-04-03 19:16:34 +0300

    feat: endless scroll added with virtuoso

--- src/pages/posts-page/ui/PostsPage.tsx
+++ src/pages/posts-page/ui/PostsPage.tsx
@@ -1,44 +1,47 @@
-import { useGetPostsQuery, type PostType } from "@/entities/post";
+import type { PostType } from "@/entities/post";
+import { useLazyGetPostsQuery } from "@/entities/post/model/api/post";
 import { Post } from "@/entities/post/ui/Post";
-import { useState } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 import { Virtuoso } from "react-virtuoso";
 
-const START_LIST = 0;
-const END_LIST = 93;
-
+const LIMIT_PER_PAGE = 10;
 export const PostsPage = () => {
-  const [startPosition, setStartPosition] = useState(0);
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fetchPosts, { isLoading }] = useLazyGetPostsQuery();
+  const firstMount = useRef(true);
 
-  const { data: posts = [] } = useGetPostsQuery({ start: startPosition });
-
-  const fetchMorePosts = () => {
-    setStartPosition((prev) => {
-      return prev + 2 < END_LIST ? prev + 2 : prev;
+  const loadMore = useCallback(async () => {
+    if (isLoading) {
+      return;
+    }
+    const { data } = await fetchPosts({
+      _page: currentPage,
+      _limit: LIMIT_PER_PAGE,
     });
-  };
+    if (!data) {
+      throw new Error("error");
+    }
 
-  const fetchPrevPosts = () => {
-    setStartPosition((prev) => {
-      return prev - 2 > START_LIST ? prev - 2 : prev;
+    setPosts((prev) => {
+      return [...prev, ...data.posts];
     });
-  };
-  const renderPosts = (index: number, post: PostType) => {
-    // const post = posts[index];
-    return <Post key={post.id} post={post} />;
-  };
+    setCurrentPage((prev) => prev + 1);
+    firstMount.current = false;
+  }, [setPosts, currentPage, isLoading]);
+
+  useEffect(() => {
+    loadMore();
+  }, []);
 
   return (
     <Virtuoso
-      style={{ display: "flex", height: "100%" }}
+      useWindowScroll
       data={posts}
-      itemContent={renderPosts}
-      startReached={fetchPrevPosts}
-      endReached={fetchMorePosts}
-
-      // increaseViewportBy={50}
-      // atBottomStateChange={(atBottom: boolean) => {
-      //   console.log(atBottom);
-      // }}
+      endReached={loadMore}
+      itemContent={(index, post) => {
+        return <Post key={post.id} post={post} />;
+      }}
     />
   );
 };
