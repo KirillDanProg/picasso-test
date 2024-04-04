e81546f47ee2907bdab94033f61d5aaa8935cf0b
KirillDanProg <kirill2898v@gmail.com>
2024-04-04 09:21:55 +0300

    feat: posts reducer added, state and dispatch types added

--- src/app/providers/store-provider/config/store.ts
+++ src/app/providers/store-provider/config/store.ts
@@ -1,10 +1,15 @@
+import { postsReducer } from "@/pages/posts-page";
 import { api } from "@/shared/api";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
+    posts: postsReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
